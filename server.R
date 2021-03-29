77b250234a4fffa28dbe4eda8747e595ebd88919
Koen Derks <koen-derks@hotmail.com>
2021-03-29 18:21:53 +0200

    Update

--- server.R
+++ server.R
@@ -47,13 +47,25 @@ if(!"shinycssloaders" %in% installed.packages())
 }
 library(shinycssloaders)
 
+if(!"gridExtra" %in% installed.packages()) 
+{ 
+    install.packages("gridExtra") 
+}
+library(gridExtra)
+
+if(!"shinyjs" %in% installed.packages()) 
+{ 
+    install.packages("shinyjs") 
+}
+library(shinyjs)
+
 #################################################################################################
 ############################ Server function ####################################################
 #################################################################################################
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-    
+
     # Define a reactive input field
     contentsrea <- reactive({
         inFile <- input$datafile
@@ -61,77 +73,110 @@ server <- function(input, output, session) {
             return(NULL)
         read.csv(inFile$datapath, header = TRUE)
     })
-    
+
     # Create an observer for the inputs
     observe({
-        updateSelectInput(session, "var1", choices = names(contentsrea()), selected = names(contentsrea())[3])
+        #updateSelectInput(session, "var1", choices = names(contentsrea()), selected = names(contentsrea())[3])
         updateSelectInput(session, "var2", choices = names(contentsrea()), selected = names(contentsrea())[4])
         updateSelectInput(session, "stratum", choices = names(contentsrea()), selected = names(contentsrea())[2])
     })
     
+    output$out <- renderUI({
+        numinputs <- lapply(1:10, function(i){
+            numericInput(inputId = paste0("n", i), label = paste0("Size stratum ", i), value = 100, min = 1)
+        })
+        shinyjs::hidden(numinputs)
+    })
+    
+    observeEvent(eventExpr = input$refresh, handlerExpr = {
+        n <- seq(length.out = as.numeric(input$num))
+        lapply(seq(10), function(i) {
+            if(i %in% n) {
+                shinyjs::show(id = paste0("n", i))
+            } else{
+                shinyjs::hide(id = paste0("n", i))
+            }
+        })
+    })
+
     # Initialize main output
     output$maintable <- renderTable({
         x <- data.frame(method = c("No stratification", "Method of moments", "Weighting", "Multilevel regression with poststratification"), x1 = rep(NA, 4), x2 = rep(NA, 4), x3 = rep(NA, 4))
         colnames(x) <- c("", "Expected relative taint", "Std. Deviation taint", "Upper bound relative taint")
         x
     }, striped = TRUE, na = ".")
-    
+
     # Initialize main moment output
     output$momentMainTable <- renderTable(NULL)
-    
+
     # Initialize main weighting output
     output$weightingMainTable <- renderTable(NULL)
-    
+
     # Initialize main post stratification output
     output$postMainTable <- renderTable(NULL)
     output$postPlot <- renderPlot(NULL)
     output$postStratumTable <- renderTable(NULL)
-    
+
     observeEvent(input$update, {
         if(!is.null(input$datafile)){
-            
-            withProgress(message = 'Analyzing', value = 0, {
-                
-                steps <- 5
-                
+
+            withProgress(message = 'Running analysis', value = 0, {
+
+                steps <- 7
+
                 #################################################################################################
                 ############################ Read data ##########################################################
                 #################################################################################################
-                
-                incProgress(1/steps, detail = "Reading data [1/5]")
-                
+
+                incProgress(1/steps, detail = "Reading data [1/7]")
+
                 df <- read.csv(input$datafile$datapath, header = TRUE)
-                ist <- df[, which(colnames(df) == input$var1)]
+                #ist <- df[, which(colnames(df) == input$var1)]
                 taint <- df[, which(colnames(df) == input$var2)]
                 stratum <- df[, which(colnames(df) == input$stratum)]
-                sample <- data.frame(ist = ist, taint = taint, stratum = stratum)
-                
+                #sample <- data.frame(ist = ist, taint = taint, stratum = stratum)
+                sample <- data.frame(taint = taint, stratum = stratum)
+
                 #################################################################################################
                 ############################ Model fitting ######################################################
                 #################################################################################################
-                
-                incProgress(1/steps, detail = "Fitting model [2/5]")
-                
-                fit <- stan_glmer(formula = taint ~ (1 | stratum), family = binomial(link = "logit"), data = sample)
-                
-                poststrat <- data.frame(stratum = 1:length(unique(sample$stratum)), N = rep(1000, length(unique(sample$stratum)) ))
-                
-                posterior_prob <- posterior_linpred(fit, transform = TRUE, newdata = poststrat)
+
+                incProgress(1/steps, detail = "Fitting model [2/7]")
+
+                fit <- stan_glmer(formula = taint ~ (1 | stratum), family = binomial(link = "logit"), data = sample, iter = input$iter)
+
+                sizes <- c(input$n1, input$n2, input$n3, input$n4, input$n5)
+                sizes <- sizes[1:length(unique(sample$stratum))]
+                poststrat <- data.frame(stratum = 1:length(sizes), N = sizes)
+
+                posterior_prob <- posterior_epred(fit, newdata = poststrat)
                 poststrat_prob <- posterior_prob %*% poststrat$N / sum(poststrat$N)
-                
+
+                # For posterior predictives
+
+                incProgress(1/steps, detail = "Computing posterior predictive [3/7]")
+
+                sample_alt <- sample %>%
+                    group_by(stratum) %>%
+                    summarise(N_errors = sum(taint), N = n()) %>%
+                    ungroup()
+
+                fit_alt <- stan_glmer(cbind(N_errors, N - N_errors) ~ (1 | stratum), family = binomial("logit"), data = sample_alt)
+                pp <- posterior_predict(fit_alt)
+
                 #################################################################################################
                 ############################ Visualization ######################################################
                 #################################################################################################
-                
-                incProgress(1/steps, detail = "Visualizing data [3/5]")
-                
+
+                incProgress(1/steps, detail = "Visualizing data [4/7]")
+
                 taint_by_stratum <- sample %>%
                     group_by(stratum) %>%
                     summarise(y_mean = mean(taint), y_sd = sqrt(mean(taint) * (1 - mean(taint)) / n())) %>%
                     ungroup()
-                
+
                 yBreaks <- pretty(c(0, 0.5), min.n = 4)
-                
+
                 compare <- ggplot(data=taint_by_stratum, aes(x=stratum, y=y_mean, group=1)) +
                     geom_ribbon(aes(ymin=y_mean-y_sd,ymax=y_mean+y_sd,x=stratum),fill='lightgrey',alpha=.7)+
                     geom_line(aes(x=stratum, y=y_mean))+
@@ -147,57 +192,63 @@ server <- function(input, output, session) {
                           axis.text.x=element_text(size=10),
                           legend.title=element_text(size=10),
                           legend.text=element_text(size=10))
-                
+
                 compare2 <- ggplot()+
                     geom_hline(yintercept = mean(sample$taint),size=.8)+
                     geom_text(aes(x = 5.2, y = mean(sample$taint)+.025, label = "No stratification"), size = 2.5)+
-                    scale_x_continuous(name = "Population") + 
+                    scale_x_continuous(name = "Population") +
                     scale_y_continuous(name = "", breaks = yBreaks, limits= range(yBreaks))+
                     theme_bw()+
                     theme(legend.position="none",
                           axis.text.y=element_text(size=10),
                           axis.text.x=element_text(colour="white"),
                           legend.title=element_text(size=10),
-                          legend.text=element_text(size=10), 
+                          legend.text=element_text(size=10),
                           axis.ticks.x = element_blank())
-                
+
                 #################################################################################################
                 ############################ Main table #########################################################
                 #################################################################################################
-                
+
+                incProgress(1/steps, detail = "Rendering main table [5/7]")
+
                 output$maintable <- renderTable({
-                    
-                    incProgress(1/steps, detail = "Rendering main table [4/5]")
-                    
+
+                    momentAlpha <- mean(sample$taint) * ( ( (mean(sample$taint) * (1 - mean(sample$taint))) / var(sample$taint)) - 1)
+                    momentBeta <- (momentAlpha * (1 - mean(sample$taint))) / mean(sample$taint)
+                    momentMean <- momentAlpha / (momentAlpha + momentBeta)
+                    #browser()
+                    momentBound <- qbeta(p = input$confidence, shape1 = momentAlpha, shape2 = momentBeta)
+
                     table <- data.frame(method = "No stratification", mle = round(mean(sample$taint), 3), sd = round(sd(sample$taint), 3), ub = qbeta(input$confidence, 1 + sum(sample$taint), 1 + length(sample$taint) - sum(sample$taint)))
-                    table <- rbind(table, data.frame(method = "Method of moments", mle = -1, sd = -1, ub = -1))
+                    table <- rbind(table, data.frame(method = "Method of moments", mle = momentMean, sd = NA, ub = momentBound))
                     table <- rbind(table, data.frame(method = "Weighting", mle = -1, sd = -1, ub = -1))
                     table <- rbind(table, data.frame(method = "Multilevel regression with poststratification", mle = round(mean(poststrat_prob), 3), sd = round(sd(poststrat_prob), 3), ub = -1))
-                    colnames(table) <- c("", "Expected relative taint", "Std. Deviation taint", "Upper bound relative taint")    
+                    colnames(table) <- c("", "Expected relative taint", "Std. Deviation taint", "Upper bound relative taint")
                     table
-                    
+
                 }, striped = T, na = ".")
-                
+
                 #################################################################################################
                 ############################ Individual strata table ############################################
                 #################################################################################################
-                
-                incProgress(1/steps, detail = "Rendering additional tables [5/5]")
-                
+
+                incProgress(1/steps, detail = "Rendering additional tables [6/7]")
+
                 stratumtable <- data.frame(
                     stratum = 1:length(unique(sample$stratum)),
                     N = rep(-1, length(unique(sample$stratum)),
                             stratum_sample = rep(-1, length(unique(sample$stratum))),
                             stratum_sample_sd = rep(-1, length(unique(sample$stratum))),
+                            stratum_population_n = rep(-1, length(unique(sample$stratum))),
                             stratum_est = rep(-1, length(unique(sample$stratum))),
                             stratum_sd = rep(-1, length(unique(sample$stratum))))
                 )
-                
+
                 for(i in 1:length(levels(as.factor(poststrat$stratum)))) {
                     poststrat_stratum <- poststrat[poststrat$stratum == i, ]
-                    posterior_prob_stratum <- posterior_linpred(
+                    posterior_prob_stratum <- posterior_epred(
                         fit,
-                        transform = TRUE,
                         draws = 1000,
                         newdata = as.data.frame(poststrat_stratum)
                     )
@@ -205,19 +256,22 @@ server <- function(input, output, session) {
                     stratumtable$N[i] <- as.integer(length(sample$taint[sample$stratum == i]))
                     stratumtable$stratum_sample[i] <- round(mean(sample$taint[sample$stratum == i]), 3)
                     stratumtable$stratum_sample_sd[i] <- round(sd(sample$taint[sample$stratum == i]), 3)
+                    stratumtable$stratum_population_n <- sizes
                     stratumtable$stratum_est[i] <- round(mean(poststrat_prob_stratum), 3)
                     stratumtable$stratum_sd[i] <- round(sd(poststrat_prob_stratum), 3)
                 }
-                
+
                 output$postStratumTable <- renderTable({
-                    colnames(stratumtable) <- c("Stratum", "Size", "Sample mu", "Sample sigma", "Model mu", "Model sigma")
+                    colnames(stratumtable) <- c("Stratum", "Sample size", "Sample mu", "Sample sigma", "Population size", "Population mu", "Population sigma")
                     stratumtable
                 })
-                
+
                 #################################################################################################
                 ############################ Additions to figure ################################################
                 #################################################################################################
-                
+
+                incProgress(1/steps, detail = "Rendering additional plots [7/7]")
+
                 output$postPlot <- renderPlot({
                     compare <- compare +
                         geom_point(data=stratumResults, mapping=aes(x=stratum, y=stratum_est),
@@ -225,17 +279,36 @@ server <- function(input, output, session) {
                         geom_line(data=stratumResults, mapping=aes(x=stratum, y=stratum_est,group=1),
                                   inherit.aes=TRUE,colour='#238b45')+
                         geom_ribbon(data=stratumResults,mapping=aes(x=stratum,ymin=stratum_est-stratum_sd,
-                                                                    ymax=stratum_est+stratum_sd,group=1), 
+                                                                    ymax=stratum_est+stratum_sd,group=1),
                                     inherit.aes=FALSE,fill='#2ca25f',alpha=.3)
-                    
+
                     compare2 <- compare2 +
                         geom_hline(yintercept = mean(poststrat_prob), colour = '#2ca25f', size = 1) +
                         geom_text(aes(x = 5.2, y = mean(poststrat_prob) - .025), label = "MRP", colour = '#2ca25f')
-                    
+
                     p <- bayesplot_grid(compare,compare2, grid_args = list(nrow=1, widths = c(8,2)))
                     p
                 })
-                
+
+                output$stratumPlot <- renderPlot({
+                    plotList <- list()
+                    for(i in 1:length(unique(sample$stratum))){
+                        dat <- as.data.frame(table(pp[, i]), stringsAsFactors = F)
+                        dat$Var1 <- as.numeric(dat$Var1)
+                        yBreaks <- pretty(c(0, max(dat$Freq)), min.n = 4)
+                        plotList[[i]] <- ggplot(data = dat, mapping = aes(x = Var1, y = Freq)) +
+                            geom_bar(fill = "darkred", colour = "black", stat = "identity") +
+                            ggtitle(paste0("Stratum: ", i)) +
+                            scale_y_continuous(name = "", limits = range(yBreaks), breaks = yBreaks) +
+                            scale_x_continuous(name = "Predicted errors") +
+                            theme_bw() +
+                            theme(axis.text.y = element_blank(),
+                                  axis.ticks.y = element_blank())
+                    }
+                    n <- length(plotList)
+                    nCol <- floor(sqrt(n))
+                    do.call("grid.arrange", c(plotList, ncol=nCol))
+                })
             })
         }
     })
--- ui.R
+++ ui.R
@@ -1,76 +1,104 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+if(!"shiny" %in% installed.packages()) 
+{ 
+    install.packages("shiny") 
+}
 library(shiny)
+
+if(!"shinyjs" %in% installed.packages()) 
+{ 
+    install.packages("shinyjs") 
+}
+library(shinyjs)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     
+    useShinyjs(),
+    
     tags$head(tags$style(
         HTML('
-                                   #sidebar {
-                                   background-color: #FA8072;
-                                   }
-                                   
-                                    #confidence {
-                                   background-color: #FA8072;
-                                   }
-                                   
-                                   body, label, input, button, select { 
-                                   font-family: "Arial";
-                                   }')
+                    #title{
+                      color: black;
+                      font-size: 30px;
+                      font-style: italic;
+                    }
+                    #text1{
+                      color: white;
+                    }
+                    #datafile{
+                      font-color: white;
+                    }
+                                 
+                   #sidebar {
+                   background-color: #1e90ff;
+                   }
+                   
+.js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: black}
+.js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: black}
+                   
+                   body, label, input, button, select { 
+                   font-family: "Arial";
+                   }')
     )),
     
-    titlePanel("Stratified evaluation of audit samples", windowTitle = "Stratification"),
+    titlePanel(title = "", windowTitle = "Stratification"),
+    p(id = "title", "Stratified evaluation of audit samples"),
     
     sidebarLayout(
         
         sidebarPanel(
             id="sidebar",
             
-            p("This is an application exploring stratification in an audit setting. Upload some data and inspect your sample. Your file must be in .csv format to upload, have ';' as the cell separator and use ',' as a decimal separator."),
+            p(id = "text1", "This is an application exploring stratification in an audit setting. Upload some data and inspect your sample. Your file must be in .csv format to upload, have ';' as the cell separator and use ',' as a decimal separator."),
             
-            fileInput(inputId = "datafile", label = "Upload a data file", multiple = FALSE, placeholder = "No file selected", accept = "csv"),
+            fileInput(inputId = "datafile", label = "Upload a data file", multiple = FALSE, placeholder = "No file selected", accept = "csv", buttonLabel = "Step 1: Data"),
             
-            selectInput(inputId = "var1", label = "Ist variable", choices = ""),
+            #selectInput(inputId = "var1", label = "Ist variable", choices = ""),
             selectInput(inputId = "var2", label = "Taint variable", choices = ""),
             selectInput(inputId = "stratum", label = "Stratum variable", choices = ""),
             
             sliderInput(inputId = "confidence", label = "Confidence for upper bound:",
-                        min = 0.8, max = 0.999,
-                        value = 0.95, step = 0.01),
+                        min = 0.8, 
+                        max = 0.999,
+                        value = 0.95, 
+                        step = 0.01),
+            sliderInput(inputId = "iter", label = "Number of iterations:", 
+                         value = 2000, 
+                         min = 500, 
+                         max = 5000, 
+                         step = 100),
             
-            actionButton(inputId = "update", label = "Go!", icon = icon("cat"))
+            numericInput(inputId = "num", label = "Number of strata", value = 1, min = 1),
+            
+            uiOutput(outputId = "out"),
+            
+            actionButton(inputId = "refresh", label = "Step 2: Stratum sizes", icon = icon("cat")),
+            actionButton(inputId = "update", label = "Step 3: Analyze", icon = icon("cat"))
         ),
         mainPanel(
             tabsetPanel(
-                tabPanel(title = "All", 
+                tabPanel(title = "All methods", 
                          icon = icon("boxes"),
                          h3("Results"),
-                         shinycssloaders::withSpinner(tableOutput(outputId = "maintable"), color = "darkred", type = 4, size = 1)
+                         shinycssloaders::withSpinner(tableOutput(outputId = "maintable"), color = "#FF4500", type = 4, size = 1)
                 ),
                 tabPanel(title = "Method of moments",
                          icon = icon("ruler-combined"),
                          h3("Results"),
-                         shinycssloaders::withSpinner(tableOutput(outputId = "momentMainTable"), color = "darkred", type = 4, size = 1)
+                         shinycssloaders::withSpinner(tableOutput(outputId = "momentMainTable"), color = "#FF4500", type = 4, size = 1)
                 ),
                 tabPanel(title = "Weighting",
                          icon = icon("balance-scale-right"),
                          h3("Results"),
-                         shinycssloaders::withSpinner(tableOutput(outputId = "weightingMainTable"), color = "darkred", type = 4, size = 1)
+                         shinycssloaders::withSpinner(tableOutput(outputId = "weightingMainTable"), color = "#FF4500", type = 4, size = 1)
                 ),
-                tabPanel(title = "Multilevel regression with poststratification",
+                tabPanel(title = "MRP",
                          icon = icon("layer-group"),
                          h3("Results"),
-                         shinycssloaders::withSpinner(tableOutput(outputId = "postMainTable"), color = "darkred", type = 4, size = 1),
-                         shinycssloaders::withSpinner(plotOutput(outputId = "postPlot"), color = "darkred", type = 4, size = 1),
-                         shinycssloaders::withSpinner(tableOutput(outputId = "postStratumTable"), color = "darkred", type = 4, size = 1)
+                         shinycssloaders::withSpinner(tableOutput(outputId = "postMainTable"), color = "#FF4500", type = 4, size = 1),
+                         shinycssloaders::withSpinner(tableOutput(outputId = "postStratumTable"), color = "#FF4500", type = 4, size = 1),
+                         shinycssloaders::withSpinner(plotOutput(outputId = "postPlot"), color = "#FF4500", type = 4, size = 1),
+                         shinycssloaders::withSpinner(plotOutput(outputId = "stratumPlot"), color = "#FF4500", type = 4, size = 1)
                 )
             )
         )
